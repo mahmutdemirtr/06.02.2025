c6ffbd94b04590d283cd091fb60ec472d4105370
mahmutdemirtr <140492186+mahmutdemirtr@users.noreply.github.com>
2025-02-11 22:42:14 +0300

    AWS Amplify Lambda fonksiyonu eklendi ve yapılandırıldı

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "0602202515142df4": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "06022025191ba1f7": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
